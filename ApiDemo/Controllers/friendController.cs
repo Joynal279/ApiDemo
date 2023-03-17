d735824dfdf3afa9d4e6ac3ef99a7286de80962a
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-03-17 21:38:06 +0600

    initial commit

---
+++ ApiDemo/Controllers/friendController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class friendController : ControllerBase
+    {
+
+        public List<Friend> GetFriends()
+        {
+            List<Friend> friends = new List<Friend>(3);
+
+            friends.Add(new Friend("Joynal", "Abedin", "Dhaka"));
+            friends.Add(new Friend("Mehedi", "Hasan", "USA"));
+            friends.Add(new Friend("Murad", "Wajed", "Australia"));
+
+            return friends;
+        }
+
+    }
+}
---
+++ ApiDemo/Friend.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ApiDemo
+{
+	public class Friend
+	{
+
+		public string firstname { get; set; }
+		public string lastname { get; set; }
+		public string location { get; set; }
+
+		public Friend()
+		{
+		}
+
+        public Friend(string firstname, string lastname, string location)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.location = location;
+        }
+    }
+}
+
